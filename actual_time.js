a2258fe979e51a308f3c2aafc5311b1569cf3461
Aleksey Khoroshev <aleksxor@gmail.com>
2013-07-23 22:42:15 +0700

    set_actual_time update

--- actual_time.js
+++ actual_time.js
@@ -12,8 +12,9 @@
     return function() {
       $el.find(selector).each(function(i, obj) {
         var time;
-        time = $el.find(obj).data('time')
-        $el.find(obj).html(moment(time).fromNow())
+        if (time = $el.find(obj).data('time')) {
+          $el.find(obj).html(moment(time).fromNow())
+        }
       });
     };
   };
@@ -35,7 +36,7 @@
       return console.log('actual_time unable to bind jQuery function. exiting...');
     }
     if (typeof $ === "function") { $ = $('body'); }
-    options = $.extend(_defaults, options)
+    options = $.extend({}, _defaults, options)
 
     _for_now_updater($, options.selector).call();
 
@@ -43,15 +44,26 @@
   };
 
   // function set data-time attr to current date/time in ISO8601
+  // options (hash)
+  //* required
   // $el - jquery object
-  this.set_actual_time = function($el) {
-    var time = moment().format();
-    // this double assignment is necessary because jquery with $.data updates only
-    // inner element's data object, but with $.attr it also shows changes in elements panel (chrome)
-    // tho when assigned with only $.attr then $.data('time') return correct value for first call
-    // but when changed with $.attr lately $.data('time') still returns old values and it changes
-    // only with $.data('time', new_value) call
-    $el.data('time', time);
-    $el.attr('data-time', time);
+  //* optional
+  // selector: Sizzle selector for elements to be updated (default: '[role="time"]')
+  this.set_actual_time = function(options) {
+    var time = moment().format(), $;
+
+    $ = options.$el;
+    options = $.extend({}, _defaults, options);
+
+    $.find(options.selector).each(function(i, obj) {
+      // this double assignment is necessary because jquery with $.data updates only
+      // inner element's data object, but with $.attr it also shows changes in elements panel (chrome)
+      // tho when assigned with only $.attr then $.data('time') return correct value for first call
+      // but when changed with $.attr lately $.data('time') still returns old values and it changes
+      // only with $.data('time', new_value) call
+
+      $.find(obj).data('time', time);
+      $.find(obj).attr('data-time', time);
+    });
   };
 }).call(this)
